2ad2f2c4ed497e3fed9719421ab67cf1b777e968
erBo <59963141+er-Bot@users.noreply.github.com>
2020-11-25 13:25:31 +0100

    Update hashtable.c

--- src/hashtable.c
+++ src/hashtable.c
@@ -90,31 +90,21 @@ void* search(hashtable_t* ht, char* k){
 	unsigned int slot = hash_fct(k);
 	hash_item_t* item = ht->items[slot];
 
-	if (item == NULL)
-	{
-		return;
-	}
-	while (item != NULL)
-	{
-		if(strcmp(item->key,k) == 0){
+	while (item != NULL) {
+		if(strcmp(item->key,k) == 0)
 			return item->value;
-		}else
-		{
-			item = item -> next;
-		}
-		
+		item = item -> next;
 	}
 	return NULL;
 }
+
 void show(hashtable_t* ht, char* k){
 	if (search(ht,k)!= NULL)
-	{
 		printf("",search(ht,k));
-	}else
-	{
+	else
 		printf("The value of this key is not available!");
-	}
 }
+
 void remove_item(hashtable_t* ht, char* k){
 
 }
