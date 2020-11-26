a111309819a4db6cabb297a6e32e542b43e2152b
alifhaqimie <64553720+alifhaqimie@users.noreply.github.com>
2020-11-26 08:59:39 +0000

    test

--- exo.c
+++ exo.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+char occurent(char str[])
+{
+    int min=strlen(str)+1 ;
+    char ch;
+    int i,j;
+    for( i = 0; i <= strlen(str); i++)
+  	{
+  	    ch=str[i];
+  	    for(j=i+1; j <= strlen(str); j++)
+  		if(str[j] == ch)
+		{
+		    if (j<min) min=j;
+  			break;
+ 		}
+	}
+	if (min!=strlen(str)+1) {
+            printf("Le premier caractere recurrent est :\t ");
+            return str[min];
 
-char occurent(char str[]);
+	}
+	return NULL;
 
-int main() {
-    printf("%c \n",occurent("bqqbca"));
+}
+
+int main()
+{
+    int i;
+    char  y[100];
+    for(i=0;i<100;i++){
+        y[i]=0;
+    }
+    printf("entrer un mot\n");
+    scanf("%s",y);
+    printf("%c",occurent(y));
     return 0;
 }
 
-char occurent(char str[]) {
-	int min=strlen(str)+1 ;
-	char ch;
-	int j;
-	for(int i = 0; i <= strlen(str); i++) {
-		ch=str[i];
-	for(j=i+1; j <= strlen(str); j++)
-		if(str[j] == ch) {
-			if (j<min) min=j;
-			break;
- 		}
-	}
-	if (min!=strlen(str)+1) 
-		return str[min];
-
-	return '\0';
-}
