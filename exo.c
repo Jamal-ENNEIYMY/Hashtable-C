8c035175c22c6783c7d99cdf5902db8780d38e8b
erBo <jammalenneiym@gmail.com>
2020-11-26 20:57:08 +0100

    mod exo.c

--- exo.c
+++ exo.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char occurent(char str[])
-{
-    int min=strlen(str)+1 ;
-    char ch;
-    int i,j;
-    for( i = 0; i <= strlen(str); i++)
-  	{
-  	    ch=str[i];
-  	    for(j=i+1; j <= strlen(str); j++)
-  		if(str[j] == ch)
-		{
-		    if (j<min) min=j;
+char occurent(char str[]) {
+	int min=strlen(str)+1 ;
+	char ch;
+	
+	for(int i = 0; i <= strlen(str); i++) {
+		ch=str[i];
+		for(int j=i+1; j <= strlen(str); j++)
+			if(str[j] == ch) {
+		    if (j<min) min = j;
   			break;
- 		}
+  		}
 	}
-	if (min!=strlen(str)+1) {
-            printf("Le premier caractere recurrent est :\t ");
-            return str[min];
-
-	}
-	return NULL;
-
+	if (min != strlen(str)+1)
+		return str[min];
+	return '\0';
 }
 
-int main()
-{
-    int i;
-    char  y[100];
-    for(i=0;i<100;i++){
-        y[i]=0;
-    }
-    printf("entrer un mot\n");
-    scanf("%s",y);
-    printf("%c",occurent(y));
-    return 0;
+int main() {
+	char  y[100];
+	printf("entrer un mot\n");
+	scanf("%s",y);
+	printf("Le premier caractere recurrent est :\t ");
+	printf("%c\n",occurent(y));
+	return 0;
 }
-
